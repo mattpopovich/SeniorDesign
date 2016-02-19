9b5db55d513cd6dc3e73133a8ddcfcb5c0acbac3
Matt Popovich <mdp5258@CCRHWP05826.psu-erie.bd.psu.edu>
2016-02-19 11:12:34 -0500

    Initial Commit

---
+++ AzoteqGUI/AzoteqGUI/Form1.vb
@@ -0,0 +1,25 @@
+﻿' @description    Visual Basic form for communicating with Arduino Uno
+'                 Arduino communicates with Azoteq IQS316 Capactivie Touch Module
+' @author         Matt Popovich (popovivch.matt@gmail.com)
+' @version        0.1
+' @created on     February 19, 2016
+' @last modified  February 19, 2016
+
+
+' With help from: http://www.instructables.com/id/Using-Visual-Basic-to-control-Arduino-Uno/
+Imports System.IO
+Imports System.IO.Ports
+Imports System.Threading
+
+
+Public Class Form1
+
+    Shared _continue As Boolean
+    Shared _serialPort As SerialPort
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
