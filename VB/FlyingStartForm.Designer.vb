0ba0897342ff4e96553d7b14896071e1165c624d
Matt Popovich <mdp5258@ENGHWP05442.psu-erie.bd.psu.edu>
2016-02-22 11:04:00 -0500

    Added Coulston's VB Code

---
+++ VB/FlyingStartForm.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FlyingStartForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(181, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(226, 73)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'FlyingStartForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(592, 166)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FlyingStartForm"
+        Me.Text = "FlyingStartForm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+End Class
---
+++ VB/StandingStartForm.vb
@@ -0,0 +1,116 @@
+﻿Public Class StandingStartForm
+    Dim CountDown As Integer
+    Const FalseStartThreshold As Single = 1.0
+    Dim RedFalseStart, GreenFalseStart, BlueFalseStart, BlackFalseStart As Boolean
+    Public Label1 As New Label()
+
+
+
+    Private Sub StandingStartForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        FalseStartButton.Visible = False
+
+        CountDown = 4
+        RedFalseStart = False
+        GreenFalseStart = False
+        BlueFalseStart = False
+        BlackFalseStart = False
+
+        SerialPort1.Open()
+        SerialPort1.DiscardInBuffer()
+        SerialPort1.DiscardOutBuffer()
+        SerialPort1.Write("r")
+        SerialPort1.Close()
+
+        Label2.Location = New System.Drawing.Point(120, 50)
+        Label2.Text = "Get Ready"
+
+    End Sub
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        Dim SerialPortString As String
+        Dim RedDistance, GreenDistance, BlueDistance, BlackDistance As Single
+        Dim TabPosition As Integer
+        Dim RemainingString As String
+
+        SerialPort1.Open()
+        SerialPort1.DiscardInBuffer()
+        SerialPort1.DiscardOutBuffer()
+
+        SerialPort1.Write("d")
+        SerialPortString = SerialPort1.ReadLine()
+
+        TabPosition = SerialPortString.IndexOf(vbTab)
+        RedDistance = Val("&H" & Mid(SerialPortString, 1, TabPosition))         ' remember VB starts indexing at 1
+        RedDistance = RedDistance * SystemConfigurationForm.RedRotsToMeters
+        RemainingString = SerialPortString.Substring(TabPosition + 1, SerialPortString.Length - TabPosition - 1)
+
+        TabPosition = RemainingString.IndexOf(vbTab)
+        GreenDistance = Val("&H" & Mid(RemainingString, 1, TabPosition))         ' remember VB starts indexing at 1
+        GreenDistance = GreenDistance * SystemConfigurationForm.GreenRotsToMeters
+        RemainingString = RemainingString.Substring(TabPosition + 1, RemainingString.Length - TabPosition - 1)
+
+        TabPosition = RemainingString.IndexOf(vbTab)
+        BlueDistance = Val("&H " & Mid(RemainingString, 1, TabPosition))         ' remember VB starts indexing at 1
+        BlueDistance = BlueDistance * SystemConfigurationForm.BlueRotsToMeters
+        RemainingString = RemainingString.Substring(TabPosition + 1, RemainingString.Length - TabPosition - 1)
+
+        BlackDistance = Val("&H" & Mid(RemainingString, 1, TabPosition))         ' remember VB starts indexing at 1
+        BlackDistance = BlackDistance * SystemConfigurationForm.BlackRotsToMeters
+
+        SerialPort1.Close()
+
+        If RedDistance > FalseStartThreshold Then
+            RedFalseStart = True
+            FalseStartButton.Visible = True
+            Label2.Location = New System.Drawing.Point(10, 50)
+            Label2.Text = "Red False Start"
+        End If
+
+        If GreenDistance > FalseStartThreshold Then
+            GreenFalseStart = True
+            FalseStartButton.Visible = True
+            Label2.Location = New System.Drawing.Point(10, 50)
+            Label2.Text = "Green False Start"
+        End If
+
+        If BlueDistance > FalseStartThreshold Then
+            BlueFalseStart = True
+            FalseStartButton.Visible = True
+            Label2.Location = New System.Drawing.Point(10, 50)
+            Label2.Text = "Blue False Start"
+        End If
+
+        If BlackDistance > FalseStartThreshold Then
+            BlackFalseStart = True
+            FalseStartButton.Visible = True
+            Label2.Location = New System.Drawing.Point(10, 50)
+            Label2.Text = "Black False Start"
+        End If
+
+        If (RedFalseStart = False) And _
+            (GreenFalseStart = False) And _
+            (BlueFalseStart = False) And _
+            (BlackFalseStart = False) Then
+            If (CountDown > 1) Then
+                CountDown = CountDown - 1
+                Label2.Location = New System.Drawing.Point(230, 50)
+                Label2.Text = CountDown
+                Beep()
+            Else
+                Dim RaceClock As New RaceClockForm
+                RaceClock.Show()
+                Me.Close()
+            End If
+        End If
+
+    End Sub
+
+    Private Sub FalseStart_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FalseStartButton.Click
+        Dim RaceConfiguration As New RaceConfigurationForm
+        RaceConfigurationForm.Show()
+        Me.Close()
+    End Sub
+End Class
