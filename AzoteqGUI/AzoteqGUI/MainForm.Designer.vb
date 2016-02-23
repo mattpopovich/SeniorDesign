ff7f49b86cdb1d70e0baca3248d61f05131d18eb
Matt Popovich <mdp5258@ENGHWP05130.psu-erie.bd.psu.edu>
2016-02-22 20:50:11 -0500

    COM port is initialized, communication is not working yet

--- AzoteqGUI/AzoteqGUI/MainForm.Designer.vb
+++ AzoteqGUI/AzoteqGUI/MainForm.Designer.vb
@@ -22,23 +22,35 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(102, 114)
+        Me.Button1.Location = New System.Drawing.Point(129, 200)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Read"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(129, 68)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 1
         '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "MainForm"
         Me.Text = "Azoteq IQS 316 GUI"
@@ -46,5 +58,7 @@ Partial Class MainForm
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
 
 End Class
--- AzoteqGUI/AzoteqGUI/MainForm.vb
+++ AzoteqGUI/AzoteqGUI/MainForm.vb
@@ -14,34 +14,109 @@ Imports System.Threading
 
 Public Class MainForm
 
-    Shared _continue As Boolean
-    Shared SerialPort1 As SerialPort
+    Dim _continue As Boolean
+    Shared _serialPort As SerialPort
 
 
 
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Have the user select which COM port the ardunio is plugged into via pop-up window
         'MessageBox.Show("Which COM port is the Arduino connected to?", "Input COM Port", MessageBoxButtons.OK, MessageBoxIcon.Question)
+        '       look at this article when i can show list boxes in message boxes: https://msdn.microsoft.com/en-us/library/9wahf8t8.aspx
 
+        Dim returnStr As String = ""
 
+        'SerialPort1.Close()
 
+        Try
+            'SerialPort1.Open()
+            SerialPort1.Close()
+            SerialPort1.PortName = "COM31" 'change com port to match your Arduino port
+            SerialPort1.BaudRate = 9600
+            SerialPort1.DataBits = 8
+            SerialPort1.Parity = Parity.None
+            SerialPort1.StopBits = StopBits.One
+            SerialPort1.Handshake = Handshake.None
+            SerialPort1.Encoding = System.Text.Encoding.Default 'very important!
+            SerialPort1.ReadTimeout = 1000     'Delay in ms
 
-        SerialPort1.Close()
-        SerialPort1.PortName = "com10" 'change com port to match your Arduino port
-        SerialPort1.BaudRate = 9600
-        SerialPort1.DataBits = 8
-        SerialPort1.Parity = Parity.None
-        SerialPort1.StopBits = StopBits.One
-        SerialPort1.Handshake = Handshake.None
-        SerialPort1.Encoding = System.Text.Encoding.Default 'very important!
+            SerialPort1.Open()
+            MessageBox.Show("COM Port Configured!", "SUCCESS")
+
+        Catch ex As Exception
+            MessageBox.Show("Error: Serial Port read timed out.", "ERROR")
+
+        End Try
+
+        'MessageBox.Show("COM Port Configured!", "SUCCESS")
+
 
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        SerialPort1.Open()
-        SerialPort1.Write("1")
-        SerialPort1.Close()
+        writeData("1")
+
+        
+        Dim read As String = receiveData()
+
+        MessageBox.Show(read)
+
+
+    End Sub
+
+
+
+    Function receiveData() As String
+        ' Receive strings from a serial port
+        '   With help from: https://msdn.microsoft.com/en-us/library/7ya7y41k.aspx
+
+        Dim returnStr As String = ""
+
+        Try
+            writeData("1")
+
+            Do
+                Dim Incoming As String = SerialPort1.ReadLine()
+                If Incoming Is Nothing Then
+                    Exit Do
+                Else
+                    returnStr &= Incoming & vbCrLf
+                End If
+            Loop
+
+        Catch ex As TimeoutException
+            returnStr = "Error: Could not open serial port."
+
+        End Try
+
+        Return returnStr
+
+    End Function
+
+
+    Sub writeData(ByVal data As String)
+
+        'Send strings to a serial port
+        '   With help from: https://msdn.microsoft.com/en-us/library/088fx85y.aspx
+
+        Try
+            'SerialPort1.Open()
+            SerialPort1.Write(data)
+            'SerialPort1.Close()
+
+        Catch ex As Exception
+            MessageBox.Show("Error: Could not write to serial port.", "ERROR")
+        End Try
+
+
+    End Sub
+
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
 
     End Sub
 End Class
+
+
+
